1ed9d626ac7310f1cf1bfce8059bdcedb7d891af
PRABHAKAR MISHRA <114891680+Prabhakarmishrafrontend@users.noreply.github.com>
2024-02-17 15:12:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+/*fs.writeFile('read.txt', "today is osm day! :)",
+(err) => {
+    console.log("files is created");
+    console.log(err);
+});*/
+
+
+// if you want to append
+/*fs.appendFile("read.txt", "hii. babu kaise ho kya kar rahe ho", 
+(err) => {
+    console.log('task completed');
+});*/
+
+
+//file read system
+/*fs.readFile("read.txt", "utf-8", 
+(err, data) => {
+    console.log(err);
+    console.log(data);
+});*/
+
